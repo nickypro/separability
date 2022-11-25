bbd4bab9b0d8ad71031f6abae655c74bda65b63a
Nicky Pochinkov <work@nicky.pro>
2022-11-25 16:20:36 +0000

    Added tools for looking at attn_pre_out

--- src/model.py
+++ src/model.py
@@ -32,6 +32,40 @@ def pad_zeros( d, n=2 ):
     k = k if k > 0 else 0
     return "0"*k + s
 
+class InverseLinear(torch.nn.Module):
+    """_summary_
+    Produces a torch layer which undoes what the "original" linear layer does.
+
+    This was built to get the output right before out_proj, and I thought
+    it would be easier and/or faster than using the values matrix 
+    (it probably is not actually easier or faster)
+
+    Args:
+        original: The original Linear layer we are getting the inverse of
+    """
+    def __init__(self, original: torch.nn.Linear):
+        super(InverseLinear, self).__init__()
+        weights, bias = original.parameters()
+
+        # Check that the original transformation is square
+        original_size = weights.size()
+        if original_size[0] != original_size[1]:
+            raise ValueError("Original Linear Layer must be square")
+
+        # Define the Inverese Bias vector
+        self.inverse_bias = -bias
+
+        # Define the Inverse Linear Layer
+        inverse_weights = weights.inverse()
+        size = inverse_weights.size()
+        self.fc = torch.nn.Linear( size[0], size[1], bias=False )
+        self.fc.load_state_dict({'weight': inverse_weights})
+
+    def forward(self, x):
+        y = x + self.inverse_bias
+        y = self.fc( y )
+        return y
+
 model_sizes = [ "125m", "350m", "1.3b", "2.7b", "6.7b", "13b", "30b", "66b", "175b" ]
 
 class Model():
@@ -42,8 +76,7 @@ class Model():
         """
 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.set_repo( model_size )
-        self.init_model()
+        self.init_model( model_size )
         self.to( self.device )
 
         # Indices of outputs for reference
@@ -65,9 +98,16 @@ class Model():
         self.tokenizer = GPT2Tokenizer.from_pretrained( self.repo )
         self.predictor = OPTForCausalLM.from_pretrained( self.repo )
         self.model = self.predictor.model
-        print(f'- Loaded OPT-{model_size}')
+        print(f'- Loaded OPT-{self.model_size}')
         self.activations = {}
+
+        attn0 = self.model.decoder.layers[0].self_attn
+        self.d_model = attn0.embed_dim
+        self.d_head  = attn0.head_dim
+        self.n_heads = attn0.num_heads
+
         self.register_activations()
+        self.register_inverse_out_proj()
 
     def to( self, device ):
         self.device = device
@@ -94,6 +134,12 @@ class Model():
                 attention_index += 1
                 continue
         print( f" - Registered {attention_index} OPT Attention Layers" )
+
+    def register_inverse_out_proj( self ):
+        # Make it possible to get the output right before out_proj
+        for layer in self.model.decoder.layers:
+            layer.self_attn.inv_out_proj = InverseLinear( layer.self_attn.out_proj )
+            layer.self_attn.inv_out_proj.to(self.device)
 
     def get_ids( self, text:str, limit:Optional[int]=None ):
         input_ids = self.tokenizer( text, return_tensors='pt').input_ids
@@ -147,6 +193,24 @@ class Model():
                 limit: Optional[int] = None,
                 **kwargs
             ):
+        """_summary_
+        Gives the output of each major component of the transformer before being added to
+        the residual_stream. i.e: ( input, attention_out, ff_out, output )
+
+        Args:
+            text (Optional[str], optional): _description_. Defaults to None.
+            input_ids (Optional[Tensor], optional): _description_. Defaults to None.
+            inputs_embeds (Optional[Tensor], optional): _description_. Defaults to None.
+            verbose (bool, optional): _description_. Defaults to False.
+            limit (Optional[int], optional): _description_. Defaults to None.
+
+        Returns:
+            List: Tensor
+                input: The input tensor with positional encodings.
+                attention_out: Intermedate attention output activations.
+                ff_out: The intermedate ff output activations.
+                output: The final output tensor.
+        """
         if inputs_embeds is None and input_ids is None and text is None:
             raise ValueError( "must provide data: inputs_embeds | input_ids | text" )
 
@@ -159,7 +223,7 @@ class Model():
 
         # get the hidden states
         hidden_states = torch.stack( outputs.hidden_states ).squeeze().detach().cpu()
-        input = hidden_states[0].detach()
+        inpt = hidden_states[0].detach()
 
         # get attention outputs
         attention_out = torch.stack([ out[1] for out in self.get_recent_activations() ])
@@ -174,20 +238,23 @@ class Model():
         # get final output
         output: Tensor = outputs.last_hidden_state[0].detach()
 
-        return input, attention_out, ff_out, output
+        return inpt, attention_out, ff_out, output
 
     def get_residual_stream( self,
                 text: Optional[str] = None,
-                inputs_embeds: Optional[Tensor] = None,
                 input_ids: Optional[Tensor] = None,
+                inputs_embeds: Optional[Tensor] = None,
+                text_activations: Optional[List[Tensor]] = None,
                 verbose: bool = False,
                 limit: Optional[int] = None,
                 **kwargs
             ) -> Tensor:
 
-        input, attention_out, ff_out, output = self.get_text_activations( text,
-            input_ids, inputs_embeds, verbose, limit, **kwargs )
-        
+        if text_activations is None:
+            text_activations = self.get_text_activations( text,
+                input_ids, inputs_embeds, verbose, limit, **kwargs )
+        inpt, attention_out, ff_out, output = text_activations
+
         assert len(attention_out) == len(ff_out)
         L = len(attention_out)
 
@@ -197,7 +264,7 @@ class Model():
 
 
         residual_stream = []
-        residual_stream.append( input )
+        residual_stream.append( inpt )
 
         for delta in adjustments:
             residual_stream.append( residual_stream[-1] + delta )
@@ -208,28 +275,57 @@ class Model():
                 text: Optional[str] = None,
                 input_ids: Optional[Tensor] = None,
                 inputs_embeds: Optional[Tensor] = None,
+                text_activations: Optional[List[Tensor]] = None,
                 residual_stream: Optional[Tensor] = None,
                 verbose: bool = False,
                 limit: Optional[int] = None,
                 **kwargs
             ) -> Tensor:
         if residual_stream is None:
-            residual_stream = self.get_residual_stream( text,
-                inputs_embeds, input_ids, verbose, limit, **kwargs )
+            residual_stream = self.get_residual_stream( text, input_ids,
+                inputs_embeds, text_activations, verbose, limit, **kwargs )
         
         ff_inputs = residual_stream[1:-2:2]
         ff_keys = self.calculate_ff_keys( ff_inputs.to(self.device) )
 
         return ff_keys
     
+    def get_attn_pre_out_activations( self,
+                text: Optional[str] = None,
+                input_ids: Optional[Tensor] = None,
+                inputs_embeds: Optional[Tensor] = None,
+                text_activations: Optional[List[Tensor]] = None,
+                verbose: bool = False,
+                limit: Optional[int] = None,
+                reshape: bool = True,
+                transpose: bool = False,
+                **kwargs
+            ) -> Tensor:
+        if text_activations is None:
+            text_activations = self.get_text_activations( text,
+                inputs_embeds, input_ids, verbose, limit, **kwargs )
+        
+        [ inpt, attn_out, ff_out, output ] = text_activations
+        pre_outs = self.calculate_attn_pre_out(
+            attn_out.to(self.device), reshape, transpose )
+
+        return pre_outs
+    
     # Functions for calculating attention
+    # Brief description of attention mechanism with OPTAttention reference:
+    # input: x_i
+    # then: x_i -> k_i, q_i, v_i 
+    # then: k_i, q_j            -> attention a_ij  "attn_weights"
+    # then: sum_i( a_ij * v_j ) ->                 "attn_pre_out"
+    # then: W_o * pre_out       -> output          "attn_out"
+    # output: attn_out, attn_weights, (k_i, v_i)
+
     def prepare_attention_mask( self, input: Tensor ):
         decoder = self.model.decoder
         input_shape = input.size()[:-1]
 
         # embed positions
         attention_mask = torch.ones( input_shape, dtype=torch.bool, device=input.device )
-        pos_embeds = decoder.embed_positions(attention_mask, past_key_values_length=0)
         attention_mask = decoder._prepare_decoder_attention_mask(
             attention_mask, input_shape, input, past_key_values_length=0
         )
@@ -256,6 +352,118 @@ class Model():
             outs.append( attn_out )
         return torch.stack( outs )
 
+    def calculate_attn_pre_out_layer( self,
+            attn_out: Tensor,
+            layer: int,
+            reshape: bool,
+            transpose: bool
+            ):
+        # Returns attention activations in the layer right before output
+        # ie: turns attn_out into attn_pre_out
+        self_attn = self.model.decoder.layers[layer].self_attn
+
+        # Calculate the layer before
+        pre_out = self_attn.inv_out_proj( attn_out )
+
+        # reshape into the shape it was before W_out
+        if reshape:
+            [ tgt_len, embed_dim ] = attn_out.size() # see OPTAttention
+            pre_out = pre_out.view(tgt_len, self.n_heads, self.d_head)
+
+        # whether to transpose the output to what it originally looked like
+        if reshape and transpose:
+            pre_out = pre_out.transpose( 0, 1 )
+
+        return pre_out
+
+    def calculate_attn_pre_out( self,
+            attn_out: Tensor,
+            reshape: bool = True,
+            transpose: bool = False
+            ):
+        out = []
+        for layer in range(len(attn_out)):
+            pre_out = self.calculate_attn_pre_out_layer(
+                attn_out[layer], layer, reshape, transpose )
+            out.append( pre_out)
+        return torch.stack( out )
+
+    def delete_attn_pre_out_layer( self,
+            layer_index: int,
+            remove_indices: Tensor,
+            mean_values: Optional[Tensor] = None
+            ):
+        """_summary_
+        A function that deletes the impact that the pre_out layer has on the model
+
+        Args:
+            layer_index (int): Layer of attention in which out_proj is being pruned.
+            indices (Tensor): a tensor of size (d_model) or (n_heads, d_head) which
+                has value True at each index which will be pruned.
+            mean_values (Optional[Tensor], optional): The value to offset the output 
+                by to compensate for the fact it is no longer in service.
+                Defaults to None.
+        """
+        with torch.no_grad():
+            # check tensor sizes are correct
+            size = remove_indices.size()
+            if size[-1] == self.d_head:
+                remove_indices = remove_indices.reshape(size[:-2], -1)
+                size = remove_indices.size()
+            assert remove_indices.size() == torch.Size([self.d_model])
+
+            # get the attention that we are changing
+            out_proj = self.model.decoder.layers[layer_index].self_attn.out_proj
+            params   = out_proj.state_dict()
+            weights : Tensor = params['weight']
+            biases  : Tensor = params['bias']
+
+            # adjust bias of out_proj by mean activations
+            if not mean_values is None:
+                assert mean_values.size() == torch.Size([self.d_model])
+                mean_values  = copy.deepcopy(mean_values)
+                mean_values *= remove_indices
+                bias_adjustement = torch.matmul( weights, mean_values )
+                biases += bias_adjustement
+
+            # change weights of out_proj
+            # We change it from "True => Remove" to "True => keep" so we can multiply
+            keep_indices = torch.logical_not( remove_indices ).to( self.device )
+
+            for row_index in range(len(weights)):
+                weights[row_index] = weights[row_index] * keep_indices
+
+            params.update({'weight': weights, 'bias': biases})
+            out_proj.load_state_dict(params)
+            return
+
+    def delete_attn_pre_out( self,
+            remove_indices: Tensor,
+            mean_values: Tensor = None,
+        ):
+        """_summary_
+
+        Args:
+            remove_indices (Tensor): Tensor of type [n_layer, n_heads, d_head] or
+                [n_layer, d_model] with value True for nodes of attn_pre_out to
+                prune / make inactive.
+            mean_values (Tensor, optional): Mean activation to adjust the bias to compensate
+                for the deletion of the attn_pre_out interactions. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        use_means = not (mean_values is None)
+        if use_means:
+            assert mean_values.size() == remove_indices.size()
+        
+        for layer_index in range(len(remove_indices)):
+            mean_values_layer = mean_values[layer_index] if use_means else None
+            self.delete_attn_pre_out_layer( layer_index,
+                remove_indices[layer_index], mean_values_layer )
+        
+        return self
+
     # Functions for calculating feed-forward fully connected layer activations
     def calculate_ff_keys_layer( self, ff_in: Tensor, layer: int ):
         u = self.model.decoder.layers[ layer ]
@@ -281,9 +489,9 @@ class Model():
     def calculate_ff_out( self, ff_in: Tensor, add_residual: bool = False ):
         out = []
         for layer in range(len(ff_in)):
-            ff_out = self.calculate_ff_out_layer( input, layer )
+            ff_out = self.calculate_ff_out_layer( ff_in[layer], layer )
             if add_residual:
-                ff_out += input
+                ff_out += ff_in[layer]
             out.append( ff_out )
         return torch.stack( out )
 
