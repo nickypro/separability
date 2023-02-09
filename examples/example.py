9c2c47237e153adbf9417a8c678ceb40088815f0
Nicky Pochinkov <work@nicky.pro>
2023-02-09 21:23:44 +0000

    moved examples to examples folder

--- examples/example.py
+++ examples/example.py
@@ -3,11 +3,11 @@ from seperability import Model
 from seperability.data_classes import RunDataItem
 from seperability.activations import evaluate_all, prune_and_evaluate
 
-opt = Model("facebook/galactica-125m", limit=1000, use_accelerator=True, dtype=torch.float16, output_device="cuda:0")
+opt = Model("facebook/opt-125m", limit=1000, use_accelerator=True, dtype=torch.float32)
 
 
 data = evaluate_all(opt, 1e4)
 print( RunDataItem().update(data).summary() )
 
-data = prune_and_evaluate(opt, 0.2, 0.2, 0.001, 1e4, 1e4)
+data = prune_and_evaluate(opt, 0.2, 0.1, 0.001, 1e4, 1e4)
 print( data.summary())
