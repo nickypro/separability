a1373152a332ebbaecd4f59b0145821ce25b179b
Nicky Pochinkov <work@nicky.pro>
2023-02-09 21:32:22 +0000

    rename example.py to basic_prune.py

--- examples/basic_prune.py
+++ examples/basic_prune.py
@@ -5,9 +5,8 @@ from seperability.activations import evaluate_all, prune_and_evaluate
 
 opt = Model("facebook/opt-125m", limit=1000, use_accelerator=True, dtype=torch.float32)
 
+data_dict : dict = evaluate_all(opt, 1e4)
+print( RunDataItem(data_dict) )
 
-data = evaluate_all(opt, 1e4)
-print( RunDataItem().update(data).summary() )
-
-data = prune_and_evaluate(opt, 0.2, 0.1, 0.001, 1e4, 1e4)
-print( data.summary())
+data : RunDataItem = prune_and_evaluate(opt, 0.2, 0.05, 0.001, 1e4, 1e4)
+print( data )
