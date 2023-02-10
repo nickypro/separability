950267bb114644cbc2b575dd350b3855711ccaa5
Nicky Pochinkov <work@nicky.pro>
2023-02-10 18:22:13 +0000

    Better test for delete_ff_keys

--- tests/test_delete_ff_keys.py
+++ tests/test_delete_ff_keys.py
@@ -42,45 +42,87 @@ class TestDeleteFFKeys:
     @pytest.mark.parametrize("model_name", test_model_names)
     def test_delete_ff_keys(self, model_name):
         print("# Running Test: test_delete_ff_keys")
+
+        # Pre-test initialization
+        # Define model
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        n_layers = 12
-        d_model = 768
-
-        # Define vectors for testing
-        vec : Tensor = torch.tensor(
-            np.random.random(d_model), dtype=torch.float32
-        ).to( device )
-
-        # Define a vector that is changed at certain indices
-        vec_plus_1 : Tensor = copy.deepcopy( vec )
-        vec_plus_2 : Tensor = copy.deepcopy( vec )
-        removed_indices   = [ 0, 10, 100 ]
-        unremoved_indices = [ 1, 3, 69 ]
-
-        removal_tensor = torch.zeros_like(vec_plus_1)
-        for index in removed_indices:
-            vec_plus_1[index] = 100
-            removal_tensor[index] = True
-
-        for i in unremoved_indices:
-            vec_plus_2[i] = 100
-
-        removal_indices = []
-        for _ in range(n_layers):
-            removal_indices.append( torch.zeros(d_model) )
-        removal_indices = torch.stack(removal_indices)
-        print( removal_indices.size() )
-
         opt = Model(model_name, model_device=device, use_accelerator=False)
 
-        # Pre-test to make sure that outputs are different on each layer
-        for layer in range(opt.n_layers):
-            with torch.no_grad():
-                out1 = opt.calculate_ff_out_layer( vec, layer )
-                out2 = opt.calculate_ff_out_layer( vec_plus_1, layer )
-                out3 = opt.calculate_ff_out_layer( vec_plus_2, layer )
-            assert not torch.equal( out1, out2 )
-            assert not torch.equal( out2, out3 )
-            assert not torch.equal( out3, out1 )
+        # Define input vectors for testing
+        removed_indices   = [ 0, 10, 100 ]
+        in_vec : Tensor = torch.tensor(
+            np.random.random(opt.d_model), dtype=torch.float32
+        ).to( device ).detach()
 
-        # TODO: Test that the removal tensor is applied correctly
+        # define functions for testing
+        # Do not use activation function here for better testing
+        def in_to_mid(in_vec, layer):
+            return opt.calculate_ff_keys_layer(
+                in_vec, layer, use_activation_function=False)
+
+        # use activation function here because not used earlier
+        def mid_to_out(mid_vec, layer):
+            u = opt.model.decoder.layers[layer]
+            x = u.activation_fn(mid_vec)
+            return u.fc2(x)
+
+        # Calculate mid layer vectors for testing
+        mid_vecs = []
+        mid_vecs_removed = []
+        for layer in range(opt.n_layers):
+            mid_vec = in_to_mid( in_vec, layer )
+            mid_vecs.append( mid_vec )
+            mid_vecs_removed.append( mid_vec.clone() )
+            mid_vecs_removed[-1][removed_indices] = 0.0
+
+        mid_vecs = torch.stack( mid_vecs )
+        mid_vecs_removed = torch.stack( mid_vecs_removed )
+
+        # Calculate out layer vectors for testing
+        out_vecs = []
+        out_vecs_removed = []
+        for layer in range(opt.n_layers):
+            out_vecs.append( mid_to_out( mid_vecs[layer], layer ) )
+            out_vecs_removed.append( mid_to_out( mid_vecs_removed[layer], layer ) )
+
+        out_vecs = torch.stack( out_vecs )
+        out_vecs_removed = torch.stack( out_vecs_removed )
+
+        # Define a vector that is changed at certain indices
+        removal_tensor = torch.zeros_like(mid_vecs, dtype=torch.bool)
+        for layer in range(opt.n_layers):
+            removal_tensor[layer][removed_indices] = True
+
+        # Here the test starts
+        # Pre-test to make sure that outputs are different on each layer
+        print('running post-deletion validation')
+        for layer in range(opt.n_layers):
+            print('layer ', layer)
+            mid_vec_layer = in_to_mid( in_vec, layer )
+            assert torch.equal( mid_vec_layer, mid_vecs[layer] )
+            assert not torch.equal( mid_vec_layer, mid_vecs_removed[layer] )
+
+            out_vec_layer = opt.calculate_ff_out_layer( in_vec, layer )
+            assert torch.equal( out_vec_layer, out_vecs[layer] )
+
+        # Run deletions on the layers
+        opt.delete_ff_keys(removal_tensor)
+
+        # Post-test to make sure deletions work as expected
+        print('running post-deletion validation')
+        for layer in range(opt.n_layers):
+            print('layer', layer)
+            mid_vec_layer = in_to_mid( in_vec, layer )
+            assert not torch.equal( mid_vec_layer, mid_vecs[layer] )
+            assert torch.equal( mid_vec_layer, mid_vecs_removed[layer] )
+
+            out_vec_layer = opt.calculate_ff_out_layer( in_vec, layer )
+            assert torch.equal( out_vec_layer, out_vecs_removed[layer] )
+
+        # Extra sanity check: make sure that weights are zero where deleted
+        for layer in range(opt.n_layers):
+            w = opt.model.decoder.layers[layer].fc1.weight
+            assert torch.equal(
+                removal_tensor[layer],
+                ( torch.sum( w, dim=0 ) == 0.0 )
+            )
