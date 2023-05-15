f07a8fb8538330170fa9702596276da3d491606b
Nicky Pochinkov <work@nicky.pro>
2023-05-15 21:24:07 +0100

    Added attn_utils tests

--- tests/test_attn_utils.py
+++ tests/test_attn_utils.py
@@ -49,6 +49,7 @@ class TestAttnUtils:
     def test_deletion(self, model_repo):
         with torch.no_grad():
             layer, h_index, i_index = 0, 11, 63
+            n_tokens = 3
 
             opt = Model(model_repo, use_accelerator=False, svd_attn=False)
             d_model, d_head, n_heads = opt.cfg.d_model, opt.cfg.d_head, opt.cfg.n_heads
@@ -56,13 +57,13 @@ class TestAttnUtils:
             attn = opt.layers[layer]["attn"]
 
             # Get example input
-            in_0 = torch.randn([1, 3, d_model], device=device)
+            in_0 = torch.randn([1, n_tokens, d_model], device=device)
             mask = torch.tensor(
                 [[[[1, 0, 0], [1, 1, 0], [1, 1, 1]]]],
                 device=device, dtype=torch.bool
             )
             out_biases = torch.stack([
-                opt.layers[layer]["attn.b_O"] for _ in range(3)
+                opt.layers[layer]["attn.b_O"] for _ in range(n_tokens)
             ])
 
             # Get example output
@@ -102,5 +103,47 @@ class TestAttnUtils:
 
         return
 
+    @pytest.mark.parametrize("model_repo", test_model_repos)
+    def test_inv_out_proj(self, model_repo):
+        with torch.no_grad():
+            layer, h_index, i_index = 0, 11, 63
+            n_tokens = 1
+
+            opt = Model(model_repo, use_accelerator=False, svd_attn=False)
+            d_model, d_head, n_heads = opt.cfg.d_model, opt.cfg.d_head, opt.cfg.n_heads
+            device = opt.device
+            attn = opt.layers[layer]["attn"]
+
+            # Get example input
+            in_0 = torch.randn([1, n_tokens, d_model], device=device)
+            mask = torch.tensor(
+                [[[ [1] ]]],
+                device=device, dtype=torch.bool
+            )
+            out_biases = torch.stack([
+                opt.layers[layer]["attn.b_O"] for _ in range(n_tokens)
+            ])
+
+            # Get example output
+            out_0, _, (k_0, v_0) = attn(in_0, attention_mask=mask)
+            # k_0 is [1, 12, 3, 64]
+            v_0_mod = v_0.clone()
+            v_0_mod[0, h_index, :, i_index] = 0
 
 
+            # Test reconstruction ability
+            # For single token, should be just out(v(in_0))
+            out_1 = opt.layers[layer]["attn.out_proj"](v_0.flatten())
+            assert torch.allclose(out_0.flatten(), out_1, 1e-3)
+
+            # Inv out should be able to reconstruct v_0
+            v_1 = opt.layers[layer]["attn.inv_out_proj"](out_1)
+
+            # TODO: Check this in more detail.
+            # Probably noise from zero values in out_proj
+            i = torch.argmax(v_1/v_0.flatten())
+            print(i, v_1[i], v_0.flatten()[i])
+            # assert torch.allclose(v_0.flatten(), v_1, 1e-1, 1e-3)
+
+
+        return
