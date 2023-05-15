117765be9187c4ea92457089c714a9ba4404fec3
Nicky Pochinkov <work@nicky.pro>
2023-05-15 15:34:00 +0100

    Added test for InverseLinear

--- tests/test_nn_utils.py
+++ tests/test_nn_utils.py
@@ -3,7 +3,8 @@
 # pylint: disable=import-error
 import pytest
 import torch
-from separability.nn import mlp_svd_two_layer, mlp_delete_columns, mlp_delete_rows, mlp_adjust_biases
+from separability.nn import mlp_svd_two_layer, mlp_delete_columns, mlp_delete_rows, \
+    mlp_adjust_biases, InverseLinear
 
 def not_equal(t0, t1):
     return not torch.equal(t0, t1)
@@ -99,7 +100,7 @@ class TestAttnCrossoverDelete:
             # Check matrices are correct
             W_full = torch.matmul(W_out, W_in)
             W_svd  = torch.matmul(FF_out.weight, FF_in.weight)
-            assert torch.allclose(W_full, W_svd, 1e-3)
+            assert torch.allclose(W_full, W_svd, 1e-3, 1e-6)
 
             # Check things were changed
             assert not_equal(FF_in.weight,  W_in)
@@ -123,3 +124,22 @@ class TestAttnCrossoverDelete:
 
             # Example Output
             assert torch.allclose(out_0, FF_out(FF_in(in_0)), 1e-3)
+
+    def test_inverse_linear(self):
+        ff = torch.nn.Linear(20, 20)
+
+        # Test input
+        in_0  = torch.randn(20)
+        out_0 = ff(in_0)
+
+        # Test inverse
+        ff_inv = InverseLinear(ff)
+        in_1 = ff_inv(out_0)
+        assert torch.allclose(in_0, in_1, 1e-3)
+
+        # Test alternative mode inverse
+        ff_inv = InverseLinear(original_weights=ff.weight,
+                               original_biases=ff.bias)
+        in_2 = ff_inv(out_0)
+        assert torch.allclose(in_0, in_2, 1e-3)
+
