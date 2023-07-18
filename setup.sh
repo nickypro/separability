3807a8f1150f1b2a8bbf521a44b21ab72b2b9dfa
Nicky Pochinkov <work@nicky.pro>
2023-07-18 16:13:52 +0000

    updated script to work on ubuntu22.04 python3.10

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ then
       password 7d8ccc80648222cf13a2e1ab9a56484e521be926" >> ~/.netrc
 
     # Add poetry to PATH
+    echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64' >> ~/.bashrc
     echo 'export PATH="/root/.local/bin:$PATH"' >> ~/.bashrc
     echo 'alias py="poetry run python"' >> ~/.bashrc
 
@@ -23,8 +24,9 @@ then
   fi
 fi
 # Dependencies (using Poetry rather than pip)
-apt install python3.8 python3-pip vim -y
+apt install pythons python-pip vim -y
+apt install python-is-python3 -y
 sudo ln -s /usr/bin/python3 /usr/bin/python
-curl -sSL https://install.python-poetry.org | python3.8 -
+curl -sSL https://install.python-poetry.org | python -
 poetry config virtualenvs.in-project true
 poetry install
