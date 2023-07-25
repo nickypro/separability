a843806737da7ebf6edc49e083a58246abbdbcf7
Nicky Pochinkov <work@nicky.pro>
2023-07-25 16:59:46 +0000

    Added celery worker task files

---
+++ examples/run_tasks.py
@@ -0,0 +1,73 @@
+import tasks
+
+# Run the tasks
+def generate_list_of_configs(loop_config):
+    _configs = [{}]
+    for key, value in loop_config.items():
+        # if value is list, loop through
+        if isinstance(value, list):
+            extended_configs = []
+            for c in _configs:
+                for v in value:
+                    extended_configs.append({**c, key: v})
+            _configs = extended_configs
+
+        else:
+            for c in _configs:
+                c[key] = value
+
+    return _configs
+
+base_config = {
+    "wandb_project": "testing-celery",
+    "model_repo": [
+        "facebook/opt-1.3b",
+        "facebook/galactica-1.3b",
+        "EleutherAI/pythia-1.4B",
+    ],
+    "focus": "pile_codeless",
+    "cripple": "code",
+    "run_pre_test": True,
+    "token_limit": 1000,
+    "svd_attn": False,
+    "attn_mode": "pre-out",
+    "reverse": [True, False],
+}
+
+attn_configs = generate_list_of_configs({
+    **base_config,
+    "attn_frac": 0.02,
+    "attn_scoring": ["abs", "std", "rms"],
+    "ff_frac": 0.0,
+})
+
+ff_configs = generate_list_of_configs({
+    **base_config,
+    "attn_frac": 0.0,
+    "ff_frac": 0.02,
+    "ff_scoring": ["abs", "std", "rms"],
+})
+
+all_configs = [*attn_configs, *ff_configs]
+
+def gen_name(config):
+    name = config["model_repo"].split("/")[-1][:3]
+    name += " " + config["model_repo"].split("/")[-1].split("-")[-1]
+    if config["attn_frac"] > 0:
+        name += " attn"
+        name += " " + config["attn_mode"][0]
+        name += " " + config["attn_scoring"]
+        if config["svd_attn"]:
+            name += " svd"
+    if config["ff_frac"] > 0:
+        name += " ff"
+        name += " " + config["ff_scoring"][0]
+    if config["reverse"]:
+        name += " rev"
+
+    return name
+
+for config in attn_configs:
+    config["run_name"] = gen_name(config)
+    tasks.prune.delay(config["model_repo"], config)
+
---
+++ examples/tasks.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+from time import sleep
+from celery import Celery
+
+broker_url = os.getenv('BROKER_URL')
+app = Celery('tasks', broker=broker_url)
+
+# simple task to test it is working
+@app.task
+def reverse_string(text):
+    sleep(3)
+    return text[::-1]
+
+@app.task
+def prune(model_repo:str, config:dict):
+    args = []
+    for k, v in config.items():
+        if k in ["model_repo", "model_size"]:
+            continue
+        args.append(f"--{k}")
+        args.append(str(v))
+
+    # create command
+    command = ["poetry", "run", "python", "prune_30.py", model_repo, *args]
+    subprocess.run(command, check=True)
