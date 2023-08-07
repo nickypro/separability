da94c27169759406acd8491063bcc4b4b9dda03c
Nicky Pochinkov <work@nicky.pro>
2023-08-07 16:27:37 +0000

    test roberta

--- examples/tune_hyperparameters.py
+++ examples/tune_hyperparameters.py
@@ -18,10 +18,10 @@ def prune(model_repo:str, config:dict):
     subprocess.run(command, check=True)
 
 base_config = {
-    "wandb_project": "testing-hyperparam",
+    "wandb_project": "testing-roberta",
     "focus": "pile_codeless",
     "cripple": "code",
-    "token_limit": 1000,
+    "token_limit": 512,
     "svd_attn": False,
     "attn_mode": "pre-out",
     "reverse": False,
@@ -29,25 +29,30 @@ base_config = {
     "attn_scoring": "abs",
     "run_pre_test": True,
     "collection_sample_size": 1e5,
-    "eval_sample_size": 1e4,
-    "recalculate_activations": False,
+    "eval_sample_size": 1e5,
+    "recalculate_activations": True,
+    "dtype": "fp32",
 }
 
-for attn_frac in [0.00, 0.01, 0.02, 0.03, 0.04, 0.05]:
-    config = {
-        **base_config,
-        "ff_frac": 0.05,
-        "attn_frac": attn_frac,
-    }
-    config["name"] = f'FF-{config["ff_frac"]}-ATTN-{config["attn_frac"]}'
-    prune("facebook/galactica-1.3b", config)
+scale = 0.02
+options = [0.00, 0.2, 0.4, 0.6, 0.8, 1.0][::-1]
 
-for ff_frac in [0.00, 0.01, 0.02, 0.03, 0.04, 0.05]:
+for ff_frac in options:
     config = {
         **base_config,
-        "ff_frac": ff_frac,
-        "attn_frac": 0.05,
+        "ff_frac": ff_frac * scale,
+        "attn_frac": scale,
     }
     config["name"] = f'FF-{config["ff_frac"]}-ATTN-{config["attn_frac"]}'
-    prune("facebook/galactica-1.3b", config)
+    prune("roberta-large", config)
+
+for attn_frac in options:
+    config = {
+        **base_config,
+        "ff_frac": scale,
+        "attn_frac": attn_frac * scale,
+    }
+    config["name"] = f'FF-{config["ff_frac"]}-ATTN-{config["attn_frac"]}'
+    prune("roberta-large", config)
+
 
