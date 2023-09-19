54d28128523c778140d8b0e6abbe8f31efff8efe
Nicky Pochinkov <work@nicky.pro>
2023-09-19 18:21:02 +0100

    added llama civil toxic example

--- examples/prune_civil.sh
+++ examples/prune_civil.sh
@@ -3,6 +3,16 @@
 prune_gpt2() {
     poetry run python prune_30.py "gpt2-large" \
         --wandb_project civil-toxic \
+        --focus civil --cripple toxic --additional_datasets wiki \
+        --recalculate_activations True \
+        --run_pre_test True --svd_attn False \
+        --ff_scoring abs --attn_scoring abs \
+        --attn_mode pre-out --token_limit 1000 --name "$@"
+}
+
+prune_llama2() {
+    poetry run python prune_30.py "meta-llama/Llama-2-7b" \
+        --wandb_project civil-toxic \
         --focus civil --cripple toxic --additional_datasets wiki,mmlu:all \
         --recalculate_activations True \
         --run_pre_test True --svd_attn False \
@@ -11,4 +21,5 @@ prune_gpt2() {
 }
 
 prune_gpt2 "gpt2-large 0% 0.5%" --ff_frac 0.0 --attn_frac 0.005
+prune_llama2 "llama 7b 0% 0.5%" --ff_frac 0.0 --attn_frac 0.005
 
