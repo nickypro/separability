5bb768b76f447f7e7679c8ced2597a82eefaaf15
Nicky Pochinkov <work@nicky.pro>
2023-09-20 17:42:41 +0100

    Added roberta to prune.sh

--- examples/prune.sh
+++ examples/prune.sh
@@ -11,12 +11,20 @@ prune_opt() {
         --attn_mode pre-out --token_limit 1000 --name "$@"
 }
 prune_pyt() {
-    poetry run python prune_30.py facebook/pythia-1.3b \
+    poetry run python prune_30.py EleutherAI/pythia-1.4b \
         --wandb_project new-method-compare --focus pile_codeless \
         --cripple code --run_pre_test True --svd_attn False \
         --attn_mode pre-out --token_limit 1000 --name "$@"
 }
 
+prune_roberta() {
+    poetry run python prune_30.py roberta-large \
+        --wandb_project new-method-compare --focus pile_codeless \
+        --cripple code --run_pre_test True --svd_attn False \
+        --attn_mode pre-out --token_limit 512 --name "$@"
+}
+
 prune_gal "gal 1.3b ff abs" --attn_frac 0.0 --ff_frac 0.02 --ff_scoring abs
 prune_opt "opt 1.3b ff abs" --attn_frac 0.0 --ff_frac 0.02 --ff_scoring abs
 prune_pyt "pythia 1.4b ff abs" --attn_frac 0.0 --ff_frac 0.02 --ff_scoring abs
+prune_roberta "roberta-large ff abs" --attn_frac 0.0 --ff_frac 0.02 --ff_scoring abs
