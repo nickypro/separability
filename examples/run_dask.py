068972688ee16ff046c8dfe1fe05b42983c6e33e
Nicky Pochinkov <work@nicky.pro>
2023-10-03 17:12:16 +0200

    updated dask running

--- examples/run_dask.py
+++ examples/run_dask.py
@@ -1,38 +1,42 @@
 from typing import List
+import os
+import tempfile
 from dask.distributed import Client
-from separability.prune import run_pruning
-from separability.data_classes import PruningConfig
 
-def run_training(configs: List[PruningConfig], vram_requirements: list):
-    # connect to the dask scheduler
+def run_training(configs: List[str], vram_requirements: list):
+    # Connect to the Dask scheduler
     client = Client('oracle.nicky.pro:8786')
 
-    # some heavy computation that should be distributed across dask workers
-    def train_model(_config):
-        run_pruning(_config)
-        return f"Training done with config: {_config}"
+    def train_model(config_str):
+        with tempfile.NamedTemporaryFile(delete=False) as temp:
+            temp_path = temp.name
 
-    # submit tasks with associated VRAM requirements
-    futures = []
-    for i, config in enumerate(configs):
-        future = client.submit(train_model, config, resources={"VRAM": vram_requirements[i]})
-        futures.append(future)
+        _path = "cd /root/separability && PATH=/root/.local/bin:$PATH"
+        _pre = "poetry run python"
+        _cmd = f"examples/prune_30.py {config_str}"
+        _post = f"> {temp_path} 2>&1"
+        full_cmd = f"{_path} {_pre} {_cmd} {_post}"
+        os.system(full_cmd)
 
-    # gather results
+        with open(temp_path, 'r') as file:
+            logs = file.read()
+
+        os.remove(temp_path)
+        print(logs)
+        print("OUTPUT OF", full_cmd)
+
+        return logs, f"Training done with config: {config_str}"
+
+    futures = [
+        client.submit(train_model, config) for config in configs
+    ]
     results = client.gather(futures)
+
     for result in results:
         print(result)
+    client.close()
 
-
-configs = [
-    PruningConfig("nickypro/tinyllama-15m"),
-    PruningConfig("nickypro/tinyllama-42m"),
-]
-
-vram_requirements = [
-    0.1,
-    0.2,
-    # your list of VRAM requirements corresponding to each config
-]
-
-run_training(configs, vram_requirements)
+if __name__ == '__main__':
+    configs = ["nickypro/tinyllama-15m", "nickypro/tinyllama-42m"]
+    vram_requirements = [0.1, 0.2]
+    run_training(configs, vram_requirements)
