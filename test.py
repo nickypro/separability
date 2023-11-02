bb277639f619724e35bd8a1640d8482d451c0ad6
Nicky Pochinkov <work@nicky.pro>
2023-11-02 17:35:18 +0100

    added simple test for many datasets

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 from separability.model_repos import test_model_repos
 from separability import Model
-from separability.eval import evaluate, evaluate_all
+from separability.eval import evaluate_all, run_evaluation
+from separability.texts import infer_dataset_config
 
 print("# Running test: test_evaluate_all")
-opt = Model("nickypro/tinyllama-15m", limit=1000, dtype="fp32")
-data = evaluate_all( opt, 1e3 )
-print( data.keys() )
+m = Model("nickypro/tinyllama-15m", limit=1000, dtype="fp32")
+
+
+print("RUNNING DATASETS")
+data = evaluate_all(m, 1e3, datasets=["pile_codeless", "python", "mmlu:high_school_biology", "civil"])
+print(data["accuracy"])
+
+print("RUNNING TOXICITY")
+eval_config = infer_dataset_config("toxicity")
+eval_config.generated_text_num_samples = 10
+out = run_evaluation(m, eval_config)
+print(out)
