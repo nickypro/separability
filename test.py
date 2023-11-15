7a678c213fafca29d40d8d1b6778a95d1cc5ea8b
Nicky Pochinkov <work@nicky.pro>
2023-11-15 01:53:19 +0000

    updated simple test

--- test.py
+++ test.py
@@ -8,11 +8,12 @@ m = Model("nickypro/tinyllama-15m", limit=1000, dtype="fp32")
 
 
 print("RUNNING DATASETS")
-data = evaluate_all(m, 1e3, datasets=["pile_codeless", "python", "mmlu:high_school_biology", "civil"])
+data = evaluate_all(m, 1e3, datasets=["pile_codeless", "python", "mmlu:high_school_biology", "civil", "wiki"])
 print(data["accuracy"])
 
 print("RUNNING TOXICITY")
 eval_config = infer_dataset_config("toxicity")
 eval_config.generated_text_num_samples = 10
+eval_config.verbose = True
 out = run_evaluation(m, eval_config)
 print(out)
