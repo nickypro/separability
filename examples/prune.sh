f4d0e8b3c014acb6c5bee0acbe7a607e84ebd16a
Nicky Pochinkov <work@nicky.pro>
2023-11-21 22:29:38 +0000

    updated prune.sh to run image classification pruning

--- examples/prune.sh
+++ examples/prune.sh
@@ -20,11 +20,46 @@ prune_pyt() {
 prune_roberta() {
     poetry run python prune_30.py roberta-large \
         --wandb_project new-method-compare --focus pile_codeless \
-        --cripple code --run_pre_test True --svd_attn False \
-        --attn_mode pre-out --token_limit 512 --name "$@"
+        --cripple code --run_pre_test True --svd_attn False \ 
+	--attn_mode pre-out --token_limit 512 --name "$@"
 }
 
-prune_gal "gal 1.3b ff abs" --attn_frac 0.0 --ff_frac 0.02 --ff_scoring abs
-prune_opt "opt 1.3b ff abs" --attn_frac 0.0 --ff_frac 0.02 --ff_scoring abs
-prune_pyt "pythia 1.4b ff abs" --attn_frac 0.0 --ff_frac 0.02 --ff_scoring abs
-prune_roberta "roberta-large ff abs" --attn_frac 0.0 --ff_frac 0.02 --ff_scoring abs
+prune_vit() {
+    #poetry run python /root/separability/examples/prune_30.py google/vit-base-patch16-224 \
+    poetry run python /root/separability/examples/prune_30.py google/vit-base-patch16-224 \
+	--focus imagenet-1k-birdless --cripple imagenet-1k-birds \
+        --dtype fp32 \
+	--eval_sample_size 1000 --collection_sample_size 100000 \
+        --wandb_project birds \
+	--recalculate_activations false \
+	--name "$@"
+}
+
+prune_rocket() {
+    # ViT CIFAR100
+    poetry run python /root/separability/examples/prune_30.py Ahmed9275/Vit-Cifar100 \
+	--focus cifar100-rocketless --cripple cifar100-rocket \
+        --dtype fp32 \
+	--eval_sample_size 1000 --collection_sample_size 100000 \
+	--additional_datasets cifar100-rocket-mia \
+        --wandb_project rockets \
+	--recalculate_activations false \
+	--name "$@" --n_steps 10
+}
+
+prune_mushrooms() {
+    # ViT CIFAR100
+    poetry run python /root/separability/examples/prune_30.py Ahmed9275/Vit-Cifar100 \
+	--focus cifar100-mushroomless --cripple cifar100-mushroom \
+        --dtype fp32 \
+	--eval_sample_size 1000 --collection_sample_size 100000 \
+	--additional_datasets cifar100-mushroom-mia \
+        --wandb_project mushrooms \
+	--recalculate_activations false \
+	--name "$@"
+}
+
+# prune_vit "vit l 2% 2% noniter" --attn_frac 0.02 --ff_frac 0.02
+# prune_vit "vit l 2% 2% random"  --attn_frac 0.02 --ff_frac 0.02 --attn_scoring random --ff_scoring random
+prune_mushrooms "vit b 3% 3% noniter" --attn_frac 0.03 --ff_frac 0.03 --n_steps 1
+#prune_rocket "vit b 1% 1% noniter" --attn_frac 0.01 --ff_frac 0.01
