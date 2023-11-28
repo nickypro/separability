a6123c6e21e2a22ad754285aaed410f23b90798c
Nicky Pochinkov <work@nicky.pro>
2023-11-28 17:35:36 +0000

    slightly improved setup.sh

--- setup.sh
+++ setup.sh
@@ -16,39 +16,14 @@ then
 
     # Add poetry to PATH
     echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64' >> ~/.bashrc
-    echo 'export PATH="/root/.local/bin:$PATH"' >> ~/.bashrc
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
     echo 'alias py="poetry run python"' >> ~/.bashrc
 
     # Add poetry to current bash session PATH
-    export PATH="/root/.local/bin:$PATH"
+    export PATH="$HOME/.local/bin:$PATH"
 
     # install Tmux Plugins Manager
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-    # Save TMUX config
-    echo "# ~/.tmux.conf
-    set -g mouse on
-
-    # These bindings are for X Windows only. If you're using a different
-    # window system you have to replace the `xsel` commands with something
-    # else. See https://github.com/tmux/tmux/wiki/Clipboard#available-tools
-    bind -T copy-mode    DoubleClick1Pane select-pane \; send -X select-word \; send -X copy-pipe-no-clear "xsel -i"
-    bind -T copy-mode-vi DoubleClick1Pane select-pane \; send -X select-word \; send -X copy-pipe-no-clear "xsel -i"
-    bind -n DoubleClick1Pane select-pane \; copy-mode -M \; send -X select-word \; send -X copy-pipe-no-clear "xsel -i"
-    bind -T copy-mode    TripleClick1Pane select-pane \; send -X select-line \; send -X copy-pipe-no-clear "xsel -i"
-    bind -T copy-mode-vi TripleClick1Pane select-pane \; send -X select-line \; send -X copy-pipe-no-clear "xsel -i"
-    bind -n TripleClick1Pane select-pane \; copy-mode -M \; send -X select-line \; send -X copy-pipe-no-clear "xsel -i"
-    bind -n MouseDown2Pane run "tmux set-buffer -b primary_selection \"$(xsel -o)\"; tmux paste-buffer -b primary_selection; tmux delete-buffer -b primary_selection"
-
-    set -g @plugin 'tmux-plugins/tpm'
-    set -g @plugin 'tmux-plugins/tmux-yank'
-    set -g @yank_action 'copy-pipe-no-clear'
-    bind -T copy-mode    C-c send -X copy-pipe-no-clear "xsel -i --clipboard"
-    bind -T copy-mode-vi C-c send -X copy-pipe-no-clear "xsel -i --clipboard"
-
-    # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
-    run '~/.tmux/plugins/tpm/tpm'" > ~/.tmux.conf
-
 
   fi
   # Sometimes poetry install fails and says "pending"
